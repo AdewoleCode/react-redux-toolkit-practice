7f6bff185c3010f6749dce6ad96a5ec9c8f0c483
ademola <adeadewole1712@gmail.com>
2022-10-20 15:27:16 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Profile from './components/Profile';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>
+        <Profile />
+        <Login />
+      </h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { configureStore } from '@reduxjs/toolkit'
+// import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+    {/* <Provider> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
